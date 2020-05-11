58d08c14edb65cddfb879d208cddb3daa5f38b0e
Philippe BODDAERT <boddaert.philippe@gmail.com>
2020-05-11 23:22:10 +0200

    Ajout Bloc3, 4 et 5 Terminale

---
+++ terminale/bloc3/data.js
@@ -0,0 +1,8 @@
+SOMMAIRE = [
+              { "url" : "../index.html", "titre" : "Sommaire"},
+              { "url" : "00.html", "titre" : "3.0 - Compétences"},
+              { "url" : "#", "titre" : "3.1 - Présentation"},
+              { "url" : "#", "titre" : "3.2 - Les processus"},
+              { "url" : "#", "titre" : "3.3 - Protocoles de routage"},
+              { "url" : "#", "titre" : "3.4 - Sécurité"}
+            ];
---
+++ terminale/bloc5/data.js
@@ -0,0 +1,10 @@
+SOMMAIRE = [
+              { "url" : "../index.html", "titre" : "Sommaire"},
+              { "url" : "00.html", "titre" : "5.0 - Compétences"},
+              { "url" : "#", "titre" : "5.1 - Présentation"},
+              { "url" : "#", "titre" : "5.2 - Algorithmes sur les arbres binaires"},
+              { "url" : "#", "titre" : "5.3 - Algorithmes sur les graphes"},
+              { "url" : "#", "titre" : "5.4 - Méthode « diviser pour régner »"},
+              { "url" : "#", "titre" : "5.5 - Programmation dynamique"},
+              { "url" : "#", "titre" : "5.6 - Recherche textuelle"}
+            ];
