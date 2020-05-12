44e196b1607ee4d683631268d1e05a5b9e3fe3a1
Philippe BODDAERT <philippe.boddaert.etu@unive-lille.fr>
2020-05-12 13:29:44 +0200

    Ajout Tri par insertion

--- premiere/bloc6/data.js
+++ premiere/bloc6/data.js
@@ -2,7 +2,9 @@ SOMMAIRE = [
               { "url" : "../index.html", "titre" : "Sommaire"},
               { "url" : "00.html", "titre" : "6.0 - Compétences"},
               { "url" : "#", "titre" : "6.1 - Présentation"},
-              { "url" : "03.html", "titre" : "6.2 - Les tris"},
+              { "url" : "03.html", "titre" : "6.2 - Les tris", "menu" : [
+                { "url" : "03-1.html", "titre" : "6.2.1 - Tri par insertion"}
+              ]},
               { "url" : "#", "titre" : "6.3 - Preuve de programme"},
               { "url" : "#", "titre" : "6.4 - Algorithmes des k plus proches voisins"},
               { "url" : "#", "titre" : "6.5 - Algorithmes gloutons"}
