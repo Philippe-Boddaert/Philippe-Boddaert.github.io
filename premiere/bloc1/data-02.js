f9284277af59022ce35e1bda7539b29b96cd0326
Philippe BODDAERT <philippe.boddaert.etu@unive-lille.fr>
2020-05-14 17:48:30 +0200

    Ajout système de navigation interne

---
+++ premiere/bloc1/data-02.js
@@ -0,0 +1,7 @@
+SOMMAIRE = [
+              { "url" : "index.html", "titre" : "Sommaire"},
+              { "url" : "#1.2.1", "titre" : "1.2.1 - Représentation d'un entier positif"},
+              { "url" : "#1.2.2", "titre" : "1.2.2 - Le système décimal"},
+              { "url" : "#1.2.3", "titre" : "1.2.3 - Le système binaire"},
+              { "url" : "#1.2.4", "titre" : "1.2.4 - Conversion décimale en binaire"},
+            ];
