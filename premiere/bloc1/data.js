6cf86952d9701bf99898f0550bdace1e9acb2d22
Philippe BODDAERT <philippe.boddaert.etu@unive-lille.fr>
2020-05-14 17:03:30 +0200

    Ajout contenu bloc1

--- premiere/bloc1/data.js
+++ premiere/bloc1/data.js
@@ -1,8 +1,8 @@
 SOMMAIRE = [
               { "url" : "../index.html", "titre" : "Sommaire"},
               { "url" : "00.html", "titre" : "1.0 - Compétences"},
-              { "url" : "#", "titre" : "1.1 - Présentation"},
-              { "url" : "#", "titre" : "1.2 - Représentation d'un entier positif"},
+              { "url" : "01.html", "titre" : "1.1 - Présentation"},
+              { "url" : "02.html", "titre" : "1.2 - Représentation d'un entier positif"},
               { "url" : "#", "titre" : "1.3 - Représentation d'un entier relatif"},
               { "url" : "#", "titre" : "1.4 - Représentation d'un nombre réel"},
               { "url" : "#", "titre" : "1.5 - Représentation des valeurs booléennes"},
