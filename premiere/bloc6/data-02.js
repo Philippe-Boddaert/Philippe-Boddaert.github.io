7be2588d5dc7c4b78370f1e07f32a9a3b29721f2
Philippe BODDAERT <philippe.boddaert.etu@unive-lille.fr>
2020-05-17 16:27:00 +0200

    Contenu bloc 6

---
+++ premiere/bloc6/data-02.js
@@ -0,0 +1,5 @@
+SOMMAIRE = [
+              { "url" : "index.html", "titre" : "Sommaire"},
+              { "url" : "#6.2.1", "titre" : "6.2.1 - Contexte"},
+              { "url" : "#6.2.2", "titre" : "6.2.2 - Définition"}
+            ];
--- premiere/bloc6/data.js
+++ premiere/bloc6/data.js
@@ -1,12 +1,13 @@
 SOMMAIRE = [
               { "url" : "../index.html", "titre" : "Sommaire"},
-              { "url" : "00.html", "titre" : "6.0 - Compétences"},
-              { "url" : "01.html", "titre" : "6.1 - Présentation"},
-              { "url" : "02.html", "titre" : "6.2 - Les tris", "menu" : [
-                { "url" : "02-1.html", "titre" : "6.2.1 - Tri par insertion" },
-                { "url" : "02-2.html", "titre" : "6.2.2 - Tri par sélection" },
+              { "url" : "00.html", "titre" : "6.0 - Objectifs"},
+              { "url" : "01.html", "titre" : "6.1 - Compétences"},
+              { "url" : "02.html", "titre" : "6.2 - Présentation"},
+              { "url" : "03.html", "titre" : "6.3 - Les tris", "menu" : [
+                { "url" : "03-1.html", "titre" : "6.3.1 - Tri par insertion" },
+                { "url" : "03-2.html", "titre" : "6.3.2 - Tri par sélection" },
               ]},
-              { "url" : "#", "titre" : "6.3 - Preuve de programme"},
-              { "url" : "#", "titre" : "6.4 - Algorithmes des k plus proches voisins"},
-              { "url" : "#", "titre" : "6.5 - Algorithmes gloutons"}
+              { "url" : "#", "titre" : "6.4 - Preuve de programme"},
+              { "url" : "#", "titre" : "6.5 - Algorithmes des k plus proches voisins"},
+              { "url" : "#", "titre" : "6.6 - Algorithmes gloutons"}
             ];
