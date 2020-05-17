fb9a7996acbbf9070c976d4d1c99093d6ca139d4
Philippe BODDAERT <philippe.boddaert.etu@unive-lille.fr>
2020-05-17 16:27:29 +0200

    Contenu bloc 4

--- terminale/bloc4/data.js
+++ terminale/bloc4/data.js
@@ -2,7 +2,7 @@ SOMMAIRE = [
               { "url" : "../index.html", "titre" : "Sommaire"},
               { "url" : "00.html", "titre" : "4.0 - Compétences"},
               { "url" : "#", "titre" : "4.1 - Présentation"},
-              { "url" : "#", "titre" : "4.2 - Calculabilité / Décidabilité"},
+              { "url" : "02.html", "titre" : "4.2 - Calculabilité / Décidabilité"},
               { "url" : "#", "titre" : "4.3 - Récursivité"},
               { "url" : "#", "titre" : "4.4 - Programmation Orienté Objet"},
               { "url" : "#", "titre" : "4.5 - Mise au point des programmes.Gestion des bugs"}
