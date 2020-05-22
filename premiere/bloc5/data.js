2e121e7d9aa88b23909e6a2ee246355a39025f73
Philippe BODDAERT <philippe.boddaert.etu@unive-lille.fr>
2020-05-22 21:38:47 +0200

    Ajout Base d'exercices

--- premiere/bloc5/data.js
+++ premiere/bloc5/data.js
@@ -7,5 +7,7 @@ SOMMAIRE = [
               { "url" : "04.html", "titre" : "5.4 - Fonctions"},
               { "url" : "05.html", "titre" : "5.5 - Structures conditionnelles"},
               { "url" : "06.html", "titre" : "5.6 - Structures itératives"},
-              { "url" : "07.html", "titre" : "5.7 - Les modules"}
+              { "url" : "07.html", "titre" : "5.7 - Les modules"},
+              { "url" : "database.html", "titre" : "Base d'exercices"},
+              { "url" : "sandbox.html", "titre" : "Bac à sable"}
             ];
