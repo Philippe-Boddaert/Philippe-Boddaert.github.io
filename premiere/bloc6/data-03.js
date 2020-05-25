a967fcc5a436084988fe750b8dfe133cd84e9535
Philippe BODDAERT <philippe.boddaert.etu@unive-lille.fr>
2020-05-25 19:14:51 +0200

    Ajout page de comparatif

--- premiere/bloc6/data-03.js
+++ premiere/bloc6/data-03.js
@@ -2,5 +2,6 @@ SOMMAIRE = [
               { "url" : "index.html", "titre" : "Sommaire"},
               { "url" : "03-1.html", "titre" : "6.3.1 - Tri par insertion" },
               { "url" : "03-2.html", "titre" : "6.3.2 - Tri par sélection" },
-              { "url" : "03-3.html", "titre" : "6.3.3 - Autres Tri" }
+              { "url" : "03-3.html", "titre" : "6.3.3 - Autres Tri" },
+              { "url" : "03-4.html", "titre" : "6.3.4 - Comparatif" }
             ];
