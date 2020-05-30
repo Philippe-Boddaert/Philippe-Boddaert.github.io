92a80829330c173741bc70ce5e5fcf5160172850
Philippe BODDAERT <philippe.boddaert.etu@unive-lille.fr>
2020-05-31 00:13:57 +0200

    Changement design

--- menu/index.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-
-//========== MENU ==========
-//-----Construction du menu
-menu = document.querySelector('.content');
-
-SOMMAIRE.forEach(item => ajouteLienMenu(menu, item));
