a8868053d596709c425ecf05fa66aa30d940806e
Philippe BODDAERT <philippe.boddaert.etu@unive-lille.fr>
2020-05-30 22:08:53 +0200

    Passage au deux Bloc Terminale

--- terminale/bloc2/data.js
+++
@@ -1,7 +0,0 @@
-SOMMAIRE = [
-              { "url" : "../index.html", "titre" : "Sommaire"},
-              { "url" : "00.html", "titre" : "2.0 - Compétences"},
-              { "url" : "#", "titre" : "2.1 - Présentation"},
-              { "url" : "#", "titre" : "2.2 - Base de données relationnelles"},
-              { "url" : "#", "titre" : "2.3 - Langage SQL"}
-            ];
--- terminale/bloc3/data.js
+++
@@ -1,8 +0,0 @@
-SOMMAIRE = [
-              { "url" : "../index.html", "titre" : "Sommaire"},
-              { "url" : "00.html", "titre" : "3.0 - Compétences"},
-              { "url" : "#", "titre" : "3.1 - Présentation"},
-              { "url" : "#", "titre" : "3.2 - Les processus"},
-              { "url" : "#", "titre" : "3.3 - Protocoles de routage"},
-              { "url" : "#", "titre" : "3.4 - Sécurité"}
-            ];
