364e80f58f0da7061d1cc268049263a4e227781f
Philippe BODDAERT <philippe.boddaert.etu@unive-lille.fr>
2020-05-30 20:02:09 +0200

    Passage au deux modes Bloc 3, 4

--- premiere/bloc4/data.js
+++
@@ -1,8 +0,0 @@
-SOMMAIRE = [
-              { "url" : "../index.html", "titre" : "Sommaire"},
-              { "url" : "00.html", "titre" : "4.0 - Compétences"},
-              { "url" : "#", "titre" : "4.1 - Présentation"},
-              { "url" : "#", "titre" : "4.2 - Modèle d'architecture de von Neumann"},
-              { "url" : "#", "titre" : "4.3 - Linux : Commandes de base"},
-              { "url" : "#", "titre" : "4.4 - Interface Homme / Machine"}
-            ];
